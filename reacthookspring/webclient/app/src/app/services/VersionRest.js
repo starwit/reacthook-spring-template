e507313b41cb798f482b094fa3523a150797926a
Cedric Rische <cedric.rische@cedsoft.de>
2023-03-24 23:21:39 +0100

    Removed double plus

--- reacthookspring/webclient/app/src/app/services/VersionRest.js
+++ reacthookspring/webclient/app/src/app/services/VersionRest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class VersionRest {
     constructor() {
-        this.baseUrl = window.location.pathname + + "/monitoring";
+        this.baseUrl = window.location.pathname + "/monitoring";
     }
 
     info = () => {
