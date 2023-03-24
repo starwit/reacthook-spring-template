30809e7212f059fa28421541155cceb4053fe2e4
Cedric Rische <cedric.rische@cedsoft.de>
2023-03-24 23:18:23 +0100

    Removed more stuff

--- reacthookspring/webclient/app/src/index.js
+++ reacthookspring/webclient/app/src/index.js
@@ -7,29 +7,13 @@ import {HashRouter as Router} from "react-router-dom";
 import "./localization/i18n";
 import {SnackbarProvider} from "notistack";
 import MainTheme from "./app/assets/themes/MainTheme";
-import BearerProvider from "./commons/BearerProvider/BearerProvider";
-import {AuthProvider} from "oidc-react";
 
 ReactDOM.render((
         <Router>
             <MainTheme>
-                <AuthProvider
-                    authority={window._env_.AUTH_AUTHORITY}
-                    clientId={window._env_.AUTH_CLIENT_ID}
-                    redirectUri={window.location.href}
-                    autoSignIn={true}
-                    automaticSilentRenew={true}
-                    scope={"openid profile"}
-                    onSignIn={() => {
-                        console.log("on signin");
-                    }}
-                >
-                    <BearerProvider>
-                        <SnackbarProvider maxSnack={5}>
-                            <App/>
-                        </SnackbarProvider>
-                    </BearerProvider>
-                </AuthProvider>
+                <SnackbarProvider maxSnack={5}>
+                    <App/>
+                </SnackbarProvider>
             </MainTheme>
         </Router>
     ),
